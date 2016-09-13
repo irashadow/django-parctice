2e751333154bdf0b55dfd88ba3212be99ab46ff2
irashadow <irashadow@gmail.com>
2016-09-13 11:12:55 +0800

    save for the next refactoring

--- stores/views.py
+++ stores/views.py
@@ -2,3 +2,18 @@ from django.shortcuts import render
 
 def home(request):
     return render(request, 'home.html')
+
+from .models import Store
+
+def store_list(request):
+    stores = Store.objects.all()
+    return render(request, 'store_list.html', {'stores': stores})
+
+from django.http import Http404
+
+def store_detail(request, pk):
+    try:
+        store = Store.objects.get(pk=pk)
+    except Store.DoesNotExist:
+        raise Http404
+    return render(request, 'store_detail.html', {'store': store})
