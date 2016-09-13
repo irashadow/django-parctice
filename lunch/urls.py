dd98a5c9efa3ff5651d727a3074fe8be3bbbc692
irashadow <irashadow@gmail.com>
2016-09-13 11:56:38 +0800

    do view-refactoring 1

--- lunch/urls.py
+++ lunch/urls.py
@@ -1,10 +1,10 @@
 from django.conf.urls import include, url
 from django.contrib import admin
-from stores.views import home, store_list, store_detail                 
+from stores.views import store_list, store_detail                 
+from pages.views import home
 
 urlpatterns = [
     url(r'^$', home, name='home'),
-    url(r'^store/$', store_list, name='store_list'),
-    url(r'^store/(?P<pk>\d+)/$', store_detail, name='store_detail'),    
+    url(r'^store/', include('stores.urls')),  
     url(r'^admin/', include(admin.site.urls)),
 ]
