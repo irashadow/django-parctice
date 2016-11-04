b4e68e3576a07b4450889627159f704455bcd4b5
irashadow <irashadow@gmail.com>
2016-11-04 14:13:12 +0800

    done by 14-django-crispy-form

--- stores/views.py
+++ stores/views.py
@@ -17,18 +17,20 @@ def store_detail(request, pk):
         raise Http404
     return render(request, 'stores/store_detail.html', {'store': store})
 
-from django.forms.models import modelform_factory
+
 from django.shortcuts import redirect, render
+from .forms import StoreForm
+
 
 def store_create(request):
-    StoreForm = modelform_factory(Store, fields=('name', 'notes',))
+    
     if request.method == 'POST':
-        form = StoreForm(request.POST)
+        form = StoreForm(request.POST, submit_title='建立') 
         if form.is_valid():
             store = form.save()
             return redirect(store.get_absolute_url())
     else:
-        form = StoreForm()
+        form = StoreForm(submit_title='建立') 
     return render(request, 'stores/store_create.html', {'form': form})
 
 def store_update(request, pk):
@@ -36,14 +38,14 @@ def store_update(request, pk):
         store = Store.objects.get(pk=pk)
     except Store.DoesNotExist:
         raise Http404
-    StoreForm = modelform_factory(Store, fields=('name', 'notes'))
+    
     if request.method == 'POST':
-        form = StoreForm(request.POST, instance=store)
+        form = StoreForm(request.POST, instance=store, submit_title='更新')
         if form.is_valid():
             store = form.save()
             return redirect(store.get_absolute_url())
     else:
-        form = StoreForm(instance=store)
+        form = StoreForm(instance=store, submit_title='更新')
     return render(request, 'stores/store_update.html', {
         'form': form, 'store': store,
     })
